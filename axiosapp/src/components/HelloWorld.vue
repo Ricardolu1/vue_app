40681f78c75f1212af404cd310f40b921dcbf3c6
Ricardo <568082704@qq.com>
2019-09-29 21:22:06 +0800

    sava

--- axiosapp/src/components/HelloWorld.vue
+++ axiosapp/src/components/HelloWorld.vue
@@ -1,46 +1,68 @@
 <template>
   <div class="hello">
     <h3>我是axiosapp，用来发送请求拦截响应</h3>
-    <button @click="getData">发送请求</button>
+    <button @click="getData">get方式发送请求</button>
+    <button @click="postData">post方式发送请求</button>
     <ul>
-      <li  v-for="item in items"> {{item}} </li>
+      <li v-for="item in items">{{item.title}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from "vue"
+import axios from "axios" 
+import qs from "qs"//qs插件的stringfy方法可以吧psot传递的参数变成form-data类型
 
-Vue.prototype.$http=axios
+
+
+
+Vue.prototype.$http = axios
 
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      items:[]
-    }
+      items: []
+    };
   },
-  methods:{
-    getData(){
-      console.log(this)
-      this.$http.get('https://cnodejs.org/api/v1/topics')
-        .then((res)=> {
+  methods: {
+    getData() {
+      console.log(this);
+      this.$http
+        .get("https://cnodejs.org/api/v1/topics?page=1&limit=15")
+        .then(res => {
           //次数的this只向的不是当前vue实例,可以用箭头函数也可以用self
-          console.log(res.data.data)
-          this.items=res.data.data
+          console.log(res.data.data);
+          this.items = res.data.data;
         })
-        .catch((err)=> {
-          console.log(err)
-        })
+        .catch(err => {
+          console.log(err);
+        });
     },
+    postData(){
+      this.$http
+        .post(url,qs.stringfy({
+            page:1,
+            limit:20
+          }))
+        .then(res => {
+          //次数的this只向的不是当前vue实例,可以用箭头函数也可以用self
+          console.log(res.data.data);
+          this.items = res.data.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -48,7 +70,7 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
+  display: block;
   margin: 0 10px;
 }
 a {
